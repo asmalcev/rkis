eb9ac1b4f8b3fa3121476f0f354ccf9070f264e7
Alexander <aleksmalcevrmg@gmail.com>
2022-04-05 19:30:45 +0300

    still trying

--- lab4/main.py
+++ lab4/main.py
@@ -1,3 +1,4 @@
+from cmath import inf
 from anytree import Node, RenderTree
 import json
 
@@ -9,14 +10,55 @@ for node in json_tree:
 	if not node['parent']:
 		tree[node['name']] = Node(
 			node['name'],
-			value=node['value'] if 'value' in node else -1
+			value=node['value'] if 'value' in node else None
 		)
 	else:
 		tree[node['name']] = Node(
 			node['name'],
 			parent=tree[node['parent']],
-			value=node['value'] if 'value' in node else -1
+			value=node['value'] if 'value' in node else None
 		)
 
+
+is_first_step_max = False
+
+metric = 0
+
+def minmax(node, maximazing):
+	global metric
+	metric += 1
+
+	if node.value is not None:
+		return node.value
+
+	children_values = [minmax(inode, level + 1) for inode in node.children]
+	node.value = (
+		max(children_values) if level % 2 == int(not is_first_step_max)
+		else min(children_values)
+	)
+	return node.value
+
+minmax(tree['root'], 0)
+
+# def minmax_ab(node, level, alpha, beta):
+# 	global metric
+# 	metric += 1
+
+# 	if node.value is not None:
+# 		return node.value
+
+# 	children_values = [minmax(inode, level + 1) for inode in node.children]
+
+# 	if level % 2 == int(not is_first_step_max):
+# 		node.value = ( max(children_values) )
+# 	else:
+# 		node.value = ( min(children_values) )
+# 	return node.value
+
+# minmax_ab(tree['root'], 0, +inf, -inf)
+
+
+print(metric)
+print('FIRST', 'MAX' if is_first_step_max else 'MIN')
 for pre, fill, node in RenderTree(tree['root']):
 	print('%s%s = %s' % (pre, node.name, node.value))
